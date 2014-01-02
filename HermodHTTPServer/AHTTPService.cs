efc1dcff89015137d8d3d498f05fa60345ca2752
Achim Friedland <github@ahzf.de>
2014-01-02 10:30:26 +0100

    Fixed bug if HTTP resource path is undefined

--- HermodHTTPServer/AHTTPService.cs
+++ HermodHTTPServer/AHTTPService.cs
@@ -75,7 +75,10 @@ namespace eu.Vanaheimr.Hermod.HTTP
         /// </summary>
         public IEnumerable<HTTPContentType>     HTTPContentTypes       { get; private set; }
 
-        public String HTTPRoot { get; set; }
+        /// <summary>
+        /// The file system root mapped to HTTP root.
+        /// </summary>
+        public String                           HTTPRoot               { get; set; }
 
         #endregion
 
@@ -466,6 +469,12 @@ namespace eu.Vanaheimr.Hermod.HTTP
         /// <param name="ResourceName">The path and name of the resource.</param>
         public HTTPResponse GetResources(String ResourcePath, String ResourceName)
         {
+
+            if (ResourcePath == null)
+                return new HTTPResponseBuilder() {
+                            HTTPStatusCode  = HTTPStatusCode.NotFound,
+                            Connection      = "close"
+                        };
 
             #region Return internal assembly resources...
 
