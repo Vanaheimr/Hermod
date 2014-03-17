29c9748a3e00334f0b316c16035dc37714e77219
Achim Friedland <github@ahzf.de>
2014-03-17 02:36:43 +0100

    Fixed build for mono

--- Hermod/Services/DNS/ResourceRecords/ADNSResourceRecord.cs
+++ Hermod/Services/DNS/ResourceRecords/ADNSResourceRecord.cs
@@ -1,8 +1,25 @@
+/*
+ * Copyright (c) 2013 Achim Friedland <achim.friedland@belectric.com>
+ * This file is part of eMI3 OICP <http://www.github.com/BelectricDrive/eMI3_OICP>
+ *
+ * Licensed under the Affero GPL license, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/agpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+#endregion
 
 namespace eu.Vanaheimr.Hermod.Services.DNS
 {
