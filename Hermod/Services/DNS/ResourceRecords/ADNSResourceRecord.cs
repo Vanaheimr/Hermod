0b7a31f7e85ee6f98659542e10b43d1efde38887
Achim Friedland <github@ahzf.de>
2014-04-23 19:05:50 +0200

    Minor updates

--- Hermod/Services/DNS/ResourceRecords/ADNSResourceRecord.cs
+++ Hermod/Services/DNS/ResourceRecords/ADNSResourceRecord.cs
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2013 Achim Friedland <achim.friedland@belectric.com>
- * This file is part of eMI3 OICP <http://www.github.com/BelectricDrive/eMI3_OICP>
+ * Copyright (c) 2010-2014, Achim 'ahzf' Friedland <achim@graphdefined.org>
+ * This file is part of Hermod <http://www.github.com/Vanaheimr/Hermod>
  *
- * Licensed under the Affero GPL license, Version 3.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.gnu.org/licenses/agpl.html
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
