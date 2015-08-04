e9b59863bd529ec3c1de69e7a95e37c4db250622
Achim Friedland <github@ahzf.de>
2015-08-04 18:03:47 +0200

    Bugfix

--- Hermod/HTTP/Server/URLMapping/HTTPStandardHandlers.cs
+++ Hermod/HTTP/Server/URLMapping/HTTPStandardHandlers.cs
@@ -295,6 +295,127 @@ namespace org.GraphDefined.Vanaheimr.Hermod.HTTP
                 ResourceAssembly = Assembly.GetCallingAssembly();
 
             HTTPServer.AddMethodCallback(HTTPMethod.GET,
+                             URITemplate + (URITemplate.EndsWith("/") ? "" : "/"),
+                             HTTPDelegate: Request =>
+                             {
+
+                                 HTTPContentType ResponseContentType = null;
+
+                                 var FilePath    = DefaultFilename.Replace("/", ".");
+                                 var FileStream  = ResourceAssembly.GetManifestResourceStream(ResourcePath + "." + FilePath);
+
+                                 if (FileStream != null)
+                                 {
+
+                                     #region Choose HTTP Content Type based on the file name extention...
+
+                                     var FileName = FilePath.Substring(FilePath.LastIndexOf("/") + 1);
+
+                                     // Get the apropriate content type based on the suffix of the requested resource
+                                     switch (FileName.Remove(0, FileName.LastIndexOf(".") + 1))
+                                     {
+                                         case "htm": ResponseContentType = HTTPContentType.HTML_UTF8; break;
+                                         case "html": ResponseContentType = HTTPContentType.HTML_UTF8; break;
+                                         case "css": ResponseContentType = HTTPContentType.CSS_UTF8; break;
+                                         case "gif": ResponseContentType = HTTPContentType.GIF; break;
+                                         case "jpg": ResponseContentType = HTTPContentType.JPEG; break;
+                                         case "jpeg": ResponseContentType = HTTPContentType.JPEG; break;
+                                         case "svg": ResponseContentType = HTTPContentType.SVG; break;
+                                         case "png": ResponseContentType = HTTPContentType.PNG; break;
+                                         case "ico": ResponseContentType = HTTPContentType.ICO; break;
+                                         case "swf": ResponseContentType = HTTPContentType.SWF; break;
+                                         case "js": ResponseContentType = HTTPContentType.JAVASCRIPT_UTF8; break;
+                                         case "txt": ResponseContentType = HTTPContentType.TEXT_UTF8; break;
+                                         default: ResponseContentType = HTTPContentType.OCTETSTREAM; break;
+                                     }
+
+                                     #endregion
+
+                                     #region Create HTTP Response
+
+                                     return new HTTPResponseBuilder()
+                                     {
+                                         HTTPStatusCode = HTTPStatusCode.OK,
+                                         ContentType = ResponseContentType,
+                                         ContentStream = FileStream,
+                                         CacheControl = "public, max-age=300",
+                                         //Expires         = "Mon, 25 Jun 2015 21:31:12 GMT",
+                                         KeepAlive = new KeepAliveType(TimeSpan.FromMinutes(5), 500),
+                                         Connection = "Keep-Alive",
+                                     };
+
+                                     #endregion
+
+                                 }
+
+                                 else
+                                 {
+
+                                     #region Try to find a appropriate customized errorpage...
+
+                                     Stream ErrorStream = null;
+
+                                     Request.BestMatchingAcceptType = Request.Accept.BestMatchingContentType(new HTTPContentType[] { HTTPContentType.HTML_UTF8, HTTPContentType.TEXT_UTF8 });
+
+                                     if (Request.BestMatchingAcceptType == HTTPContentType.HTML_UTF8)
+                                     {
+                                         ResponseContentType = HTTPContentType.HTML_UTF8;
+                                         ErrorStream = ResourceAssembly.GetManifestResourceStream(ResourcePath.Substring(0, ResourcePath.LastIndexOf(".")) + ".ErrorPages." + "404.html");
+                                     }
+
+                                     else if (Request.BestMatchingAcceptType == HTTPContentType.TEXT_UTF8)
+                                     {
+                                         ResponseContentType = HTTPContentType.TEXT_UTF8;
+                                         ErrorStream = ResourceAssembly.GetManifestResourceStream(ResourcePath.Substring(0, ResourcePath.LastIndexOf(".")) + ".ErrorPages." + "404.txt");
+                                     }
+
+                                     else if (Request.BestMatchingAcceptType == HTTPContentType.JSON_UTF8)
+                                     {
+                                         ResponseContentType = HTTPContentType.JSON_UTF8;
+                                         ErrorStream = ResourceAssembly.GetManifestResourceStream(ResourcePath.Substring(0, ResourcePath.LastIndexOf(".")) + ".ErrorPages." + "404.js");
+                                     }
+
+                                     else if (Request.BestMatchingAcceptType == HTTPContentType.XML_UTF8)
+                                     {
+                                         ResponseContentType = HTTPContentType.XML_UTF8;
+                                         ErrorStream = ResourceAssembly.GetManifestResourceStream(ResourcePath.Substring(0, ResourcePath.LastIndexOf(".")) + ".ErrorPages." + "404.xml");
+                                     }
+
+                                     else if (Request.BestMatchingAcceptType == HTTPContentType.ALL)
+                                     {
+                                         ResponseContentType = HTTPContentType.HTML_UTF8;
+                                         ErrorStream = ResourceAssembly.GetManifestResourceStream(ResourcePath.Substring(0, ResourcePath.LastIndexOf(".")) + ".ErrorPages." + "404.html");
+                                     }
+
+                                     if (ErrorStream != null)
+                                         return new HTTPResponseBuilder()
+                                         {
+                                             HTTPStatusCode = HTTPStatusCode.NotFound,
+                                             ContentType = ResponseContentType,
+                                             ContentStream = ErrorStream,
+                                             CacheControl = "no-cache",
+                                             Connection = "close",
+                                         };
+
+                                     #endregion
+
+                                     #region ...or send a default error page!
+
+                                     else
+                                         return new HTTPResponseBuilder()
+                                         {
+                                             HTTPStatusCode = HTTPStatusCode.NotFound,
+                                             CacheControl = "no-cache",
+                                             Connection = "close",
+                                         };
+
+                                     #endregion
+
+                                 }
+
+                             });
+
+            HTTPServer.AddMethodCallback(HTTPMethod.GET,
                                          URITemplate + (URITemplate.EndsWith("/") ? "{ResourceName}" : "/{ResourceName}"),
                                          HTTPDelegate: Request => {
 
