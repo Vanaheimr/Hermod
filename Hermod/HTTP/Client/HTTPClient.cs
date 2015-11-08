c74d96d90ebe5a56654efd33718a1d72f70a4242
Achim Friedland <github@ahzf.de>
2015-11-08 22:22:31 +0100

    Timing issues

--- Hermod/HTTP/Client/HTTPClient.cs
+++ Hermod/HTTP/Client/HTTPClient.cs
@@ -494,6 +494,8 @@ namespace org.GraphDefined.Vanaheimr.Hermod.HTTP
 
                     #region Wait timeout for the server to react!
 
+                    Debug.WriteLine("[" + DateTime.Now + "] HTTPClient timeout: " + Timeout.Value.ToString());
+
                     while (!TCPStream.DataAvailable)
                     {
 
