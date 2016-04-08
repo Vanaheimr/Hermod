6286f1754d23d684a9cf6626653686e3773ae885
Achim Friedland <github@ahzf.de>
2016-04-08 23:36:33 +0200

    Minor updates

--- Hermod/DNS/ResourceRecords/ADNSResourceRecord.cs
+++ Hermod/DNS/ResourceRecords/ADNSResourceRecord.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2016, Achim 'ahzf' Friedland <achim@graphdefined.org>
+ * Copyright (c) 2010-2016, Achim 'ahzf' Friedland <achim.friedland@graphdefined.com>
  * This file is part of Vanaheimr Hermod <http://www.github.com/Vanaheimr/Hermod>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
