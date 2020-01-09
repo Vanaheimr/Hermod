348adb5be92820d0e156fc467deb58ad41c83c1e
Achim Friedland <github@ahzf.de>
2020-01-09 23:34:58 +0100

    Happy new year!

--- Hermod/Properties/AssemblyInfo.cs
+++ Hermod/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GraphDefined GmbH")]
 [assembly: AssemblyProduct("Hermod")]
-[assembly: AssemblyCopyright("Copyright © 2010-2014 GraphDefined GmbH")]
+[assembly: AssemblyCopyright("Copyright © 2010-2020 GraphDefined GmbH")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
