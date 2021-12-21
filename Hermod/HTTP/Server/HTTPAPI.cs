7b9d83af927675869cdca33c46401d778550c0ee
Achim Friedland <achim.friedland@graphdefined.com>
2021-12-21 23:38:59 +0100

    Add ReloadFinished switch

--- Hermod/HTTP/Server/HTTPAPI.cs
+++ Hermod/HTTP/Server/HTTPAPI.cs
@@ -1057,6 +1057,11 @@ namespace org.GraphDefined.Vanaheimr.Hermod.HTTP
 
 
         /// <summary>
+        /// Whether the reload of the system is finished.
+        /// </summary>
+        public Boolean                  ReloadFinished              { get; protected set; }
+
+        /// <summary>
         /// The maintenance interval.
         /// </summary>
         public TimeSpan                 MaintenanceEvery            { get; }
@@ -1802,7 +1807,7 @@ namespace org.GraphDefined.Vanaheimr.Hermod.HTTP
 
         private void DoMaintenanceSync(Object State)
         {
-            if (!DisableMaintenanceTasks)
+            if (ReloadFinished && !DisableMaintenanceTasks)
                 DoMaintenance(State).Wait();
         }
 
