8f33644db7397e8e2a6960e1b703318a92bed51f
Achim Friedland <achim.friedland@graphdefined.com>
2023-07-20 02:36:30 +0200

    Add .NET minimal WebAPIs for further HTTP tests.

--- HermodTests/HTTP/HTTPClientTests.cs
+++ HermodTests/HTTP/HTTPClientTests.cs
@@ -249,6 +249,46 @@ namespace org.GraphDefined.Vanaheimr.Hermod.UnitTests.HTTP
         #endregion
 
 
+        #region HTTPClientTest_Concurrent_001()
+
+        [Test]
+        public async Task HTTPClientTest_Concurrent_001()
+        {
+
+            var startTime     = Timestamp.Now;
+            var httpRequests  = new List<Task<HTTPResponse>>();
+
+            for (var i = 0; i < 100; i++)
+            {
+                httpRequests.Add(new HTTPClient(URL.Parse("http://127.0.0.1:82")).
+                                         POST(HTTPPath.Root + "mirror" + "httpBody",
+                                              request => {
+                                                  request.ContentType  = HTTPContentType.TEXT_UTF8;
+                                                  request.Content      = i.ToString().ToUTF8Bytes();//.PadLeft(4, '0').ToUTF8Bytes();
+                                              }));
+            }
+
+            var responeses      = await Task.WhenAll(httpRequests.ToArray());
+
+            var runtime         = Timestamp.Now - startTime;
+
+            var responseTuples  = responeses.    Select (response => new Tuple<String, String, TimeSpan>(response.HTTPRequest?.HTTPBodyAsUTF8String ?? "xxxx",
+                                                                                                         response.             HTTPBodyAsUTF8String ?? "yyyy",
+                                                                                                         response.Runtime)).
+                                                 ToArray();
+
+            var responseErrors  = responseTuples.Where  (tuple    => tuple.Item1 != tuple.Item2.Reverse()).
+                                                 ToArray();
+
+            var minRuntime      = responseTuples.Min    (tuple   => tuple.Item3.TotalMicroseconds);
+            var maxRuntime      = responseTuples.Max    (tuple   => tuple.Item3.TotalMicroseconds);
+            var avgRuntime      = responseTuples.Average(tuple   => tuple.Item3.TotalMicroseconds);
+
+        }
+
+        #endregion
+
+
     }
 
 }
