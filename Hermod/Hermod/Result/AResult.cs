91a4f3349eca4d2eed4e0d49fecda286778b2bbd
Achim Friedland <achim.friedland@graphdefined.com>
2023-08-16 21:09:17 +0200

    Reorganization

--- Hermod/Hermod/Result/AResult.cs
+++ Hermod/Hermod/Result/AResult.cs
@@ -20,28 +20,11 @@
 using Newtonsoft.Json.Linq;
 
 using org.GraphDefined.Vanaheimr.Illias;
-using System.Collections;
 
 #endregion
 
 namespace org.GraphDefined.Vanaheimr.Hermod
 {
-
-    public enum AddedOrIgnored
-    {
-        Ignored,
-        Added
-    }
-
-    public enum AddedOrUpdated
-    {
-        NoOperation,
-        Add,
-        Update,
-        Enqueued,
-        Failed
-    }
-
 
     /// <summary>
     /// An abstract result.
@@ -101,7 +84,7 @@ namespace org.GraphDefined.Vanaheimr.Hermod
 
 
 
-        public PushDataResultTypes  Result         { get; }
+        public CommandResults  Result         { get; }
         public Object?              SendPOIData    { get; }
 
 
@@ -142,7 +125,7 @@ namespace org.GraphDefined.Vanaheimr.Hermod
         /// <param name="Argument"></param>
         /// <param name="Description"></param>
         public AResult(T                      Entity,
-                       PushDataResultTypes    Result,
+                       CommandResults    Result,
                        EventTracking_Id?      EventTrackingId   = null,
                        IId?                   AuthId            = null,
                        Object?                SendPOIData       = null,
@@ -190,284 +173,6 @@ namespace org.GraphDefined.Vanaheimr.Hermod
 
     }
 
-    /// <summary>
-    /// An abstract result.
-    /// </summary>
-    /// <typeparam name="TEntity">The type of the entity.</typeparam>
-    /// <typeparam name="TId">The type of the entity identification.</typeparam>
-    public abstract class AEnitityResult<TEntity, TId> : AResult<TEntity>
-
-        where TEntity : class, IHasId<TId>
-        where TId     : IId
-
-    {
-
-        #region Properties
-
-        /// <summary>
-        /// The unique identification of the entity.
-        /// </summary>
-        public TId?  Identification    { get; }
-
-        #endregion
-
-        #region Constructor(s)
-
-        /// <summary>
-        /// Create a new abstract result.
-        /// </summary>
-        /// <param name="Entity">The entity of the operation.</param>
-        /// <param name="EventTrackingId">The unique event tracking identification for correlating this request with other events.</param>
-        /// <param name="IsSuccess">Whether the operation was successful, or not.</param>
-        /// <param name="Argument"></param>
-        /// <param name="ErrorDescription"></param>
-        public AEnitityResult(TEntity?          Entity,
-                              EventTracking_Id  EventTrackingId,
-                              Boolean           IsSuccess,
-                              String?           Argument           = null,
-                              I18NString?       ErrorDescription   = null)
-
-            : base(Entity,
-                   EventTrackingId,
-                   IsSuccess,
-                   Argument,
-                   ErrorDescription)
-
-        {
-
-            this.Identification = Entity is not null
-                                      ? Entity.Id
-                                      : default;
-
-        }
-
-
-
-
-        /// <summary>
-        /// Create a new abstract result.
-        /// </summary>
-        /// <param name="Entity">The entity of the operation.</param>
-        /// <param name="EventTrackingId">The unique event tracking identification for correlating this request with other events.</param>
-        /// <param name="IsSuccess">Whether the operation was successful, or not.</param>
-        /// <param name="Argument"></param>
-        /// <param name="ErrorDescription"></param>
-        public AEnitityResult(TEntity                Entity,
-                              PushDataResultTypes    Result,
-                              EventTracking_Id?      EventTrackingId   = null,
-                              IId?                   AuthId            = null,
-                              Object?                SendPOIData       = null,
-                              I18NString?            Description       = null,
-                              IEnumerable<Warning>?  Warnings          = null,
-                              TimeSpan?              Runtime           = null)
-
-            : base(Entity,
-                   Result,
-                   EventTrackingId,
-                   AuthId,
-                   SendPOIData,
-                   Description,
-                   Warnings,
-                   Runtime)
-
-        {
-
-            this.Identification = Entity is not null
-                                      ? Entity.Id
-                                      : default;
-
-        }
-
-        /// <summary>
-        /// Create a new abstract result.
-        /// </summary>
-        /// <param name="Entity">The entity of the operation.</param>
-        /// <param name="EventTrackingId">The unique event tracking identification for correlating this request with other events.</param>
-        /// <param name="IsSuccess">Whether the operation was successful, or not.</param>
-        /// <param name="Argument"></param>
-        /// <param name="ErrorDescription"></param>
-        public AEnitityResult(TId                    EntityId,
-                              PushDataResultTypes    Result,
-                              EventTracking_Id?      EventTrackingId   = null,
-                              IId?                   AuthId            = null,
-                              Object?                SendPOIData       = null,
-                              I18NString?            Description       = null,
-                              IEnumerable<Warning>?  Warnings          = null,
-                              TimeSpan?              Runtime           = null)
-
-            : base(default,
-                   Result,
-                   EventTrackingId,
-                   AuthId,
-                   SendPOIData,
-                   Description,
-                   Warnings,
-                   Runtime)
-
-        {
-
-            this.Identification = EntityId;
-
-        }
-
-
-        /// <summary>
-        /// Create a new abstract result.
-        /// </summary>
-        /// <param name="EntityId">The object of the operation.</param>
-        /// <param name="EventTrackingId">The unique event tracking identification for correlating this request with other events.</param>
-        /// <param name="IsSuccess">Whether the operation was successful, or not.</param>
-        /// <param name="Argument"></param>
-        /// <param name="ErrorDescription"></param>
-        public AEnitityResult(TId               EntityId,
-                              EventTracking_Id  EventTrackingId,
-                              Boolean           IsSuccess,
-                              String?           Argument           = null,
-                              I18NString?       ErrorDescription   = null)
-
-            : base(default,
-                   EventTrackingId,
-                   IsSuccess,
-                   Argument,
-                   ErrorDescription)
-
-        {
-
-            this.Identification = EntityId;
-
-        }
-
-        #endregion
-
-    }
-
-    public enum PushDataResultTypes
-    {
-
-        /// <summary>
-        /// The result is unknown and/or should be ignored.
-        /// </summary>
-        Unspecified,
-
-        /// <summary>
-        /// The service was disabled by the administrator.
-        /// </summary>
-        AdminDown,
-
-        /// <summary>
-        /// No operation e.g. because no EVSE data passed the EVSE filter.
-        /// </summary>
-        NoOperation,
-
-        /// <summary>
-        /// The data has been enqueued for later transmission.
-        /// </summary>
-        Enqueued,
-
-        /// <summary>
-        /// Success.
-        /// </summary>
-        Success,
-
-        /// <summary>
-        /// Exists already.
-        /// </summary>
-        Exists,
-
-        /// <summary>
-        /// Out-Of-Service.
-        /// </summary>
-        OutOfService,
-
-        /// <summary>
-        /// A lock timeout occured.
-        /// </summary>
-        LockTimeout,
-
-        /// <summary>
-        /// A timeout occured.
-        /// </summary>
-        Timeout,
-
-        /// <summary>
-        /// The operation led to an error.
-        /// </summary>
-        Error,
-
-        ArgumentError,
-
-        CanNotBeRemoved
-
-    }
-
-
-    /// <summary>
-    /// An abstract result.
-    /// </summary>
-    /// <typeparam name="TEntity">The type of the entity.</typeparam>
-    /// <typeparam name="TId">The type of the entity identification.</typeparam>
-    public abstract class AEnititiesResult<TResult, TEntity, TId>
-
-        where TResult : AEnitityResult<TEntity, TId>
-        where TEntity : class, IHasId<TId>
-        where TId     : IId
-
-    {
-
-        #region Properties
-
-        public PushDataResultTypes   Result             { get; }
-        public IEnumerable<TResult>  SuccessfulItems    { get; }
-        public IEnumerable<TResult>  RejectedItems      { get; }
-
-        public IId?                  AuthId             { get; }
-        public Object?               SendPOIData        { get; }
-        public EventTracking_Id      EventTrackingId    { get; }
-        public I18NString            Description        { get; }
-        public IEnumerable<Warning>  Warnings           { get; }
-        public TimeSpan              Runtime            { get; }
-
-        #endregion
-
-        #region Constructor(s)
-
-        /// <summary>
-        /// Create a new abstract result.
-        /// </summary>
-        /// <param name="Entity">The entity of the operation.</param>
-        /// <param name="EventTrackingId">The unique event tracking identification for correlating this request with other events.</param>
-        /// <param name="IsSuccess">Whether the operation was successful, or not.</param>
-        /// <param name="Argument"></param>
-        /// <param name="ErrorDescription"></param>
-        public AEnititiesResult(PushDataResultTypes    Result,
-                                IEnumerable<TResult>?  SuccessfulEVSEs   = null,
-                                IEnumerable<TResult>?  RejectedEVSEs     = null,
-                                IId?                   AuthId            = null,
-                                Object?                SendPOIData       = null,
-                                EventTracking_Id?      EventTrackingId   = null,
-                                I18NString?            Description       = null,
-                                IEnumerable<Warning>?  Warnings          = null,
-                                TimeSpan?              Runtime           = null)
-        {
-
-            this.Result           = Result;
-            this.SuccessfulItems  = SuccessfulEVSEs ?? Array.Empty<TResult>();
-            this.RejectedItems    = RejectedEVSEs   ?? Array.Empty<TResult>();
-            this.AuthId           = AuthId;
-            this.SendPOIData      = SendPOIData;
-            this.EventTrackingId  = EventTrackingId ?? EventTracking_Id.New;
-            this.Description      = Description     ?? I18NString.Empty;
-            this.Warnings         = Warnings        ?? Array.Empty<Warning>();
-            this.Runtime          = Runtime         ?? TimeSpan.Zero;
-
-        }
-
-        #endregion
-
-
-    }
-
-
 
     /// <summary>
     /// An abstract result.
@@ -475,6 +180,7 @@ namespace org.GraphDefined.Vanaheimr.Hermod
     /// <typeparam name="T1">The type of the result.</typeparam>
     /// <typeparam name="T2">The type of the result.</typeparam>
     public abstract class AResult<T1, T2>
+
     {
 
         #region Properties
@@ -557,5 +263,4 @@ namespace org.GraphDefined.Vanaheimr.Hermod
                                       : "!");
 
     }
-
 }
