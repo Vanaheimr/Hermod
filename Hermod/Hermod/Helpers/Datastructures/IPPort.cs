11db885158be2e48f74f3ce4b5786ab707dcaaa0
Achim Friedland <achim.friedland@graphdefined.com>
2025-03-06 23:09:04 +0100

    Fixed typo

--- Hermod/Hermod/Helpers/Datastructures/IPPort.cs
+++ Hermod/Hermod/Helpers/Datastructures/IPPort.cs
@@ -122,7 +122,7 @@ namespace org.GraphDefined.Vanaheimr.Hermod
 
         /// <summary>
         /// Create an automatic Internet Protocol Layer 4 Port,
-        /// which will be choosen by .NET during socket creation!
+        /// which will be chosen by .NET during socket creation!
         /// </summary>
         public static IPPort Auto
             => new (0);
