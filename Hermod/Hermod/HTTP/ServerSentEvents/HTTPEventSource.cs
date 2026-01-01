2529fe5d35bf5c9455780f06a885f7cfaa1032a8
Achim Friedland <achim.friedland@graphdefined.com>
2026-01-01 15:15:49 +0100

    Happy new year!

--- Hermod/Hermod/HTTP/ServerSentEvents/HTTPEventSource.cs
+++ Hermod/Hermod/HTTP/ServerSentEvents/HTTPEventSource.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 2010-2025 GraphDefined GmbH <achim.friedland@graphdefined.com>
+ * Copyright (c) 2010-2026 GraphDefined GmbH <achim.friedland@graphdefined.com>
  * Author: Achim Friedland <achim.friedland@graphdefined.com>
  * This file is part of Vanaheimr Hermod <https://www.github.com/Vanaheimr/Hermod>
  *
